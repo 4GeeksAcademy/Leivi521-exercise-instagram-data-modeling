2a7cf55b92d1001b72a8cfbe06d590a3946f4897
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-07-23 20:17:24 +0000

    almost done

--- src/models.py
+++ src/models.py
@@ -1,29 +1,100 @@
+# import os
+# import sys
+# from sqlalchemy import Column, ForeignKey, Integer, String
+# from sqlalchemy.orm import relationship, declarative_base
+# from sqlalchemy import create_engine
+# from eralchemy2 import render_er
+
+# Base = declarative_base()
+
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+#     def to_dict(self):
+#         return {}
+
+# ## Draw from SQLAlchemy base
+# try:
+#     result = render_er(Base, 'diagram.png')
+#     print("Success! Check the diagram.png file")
+# except Exception as e:
+#     print("There was a problem genering the diagram")
+#     raise e
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+follower = Table(
+    "follower",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id")),
+    Column("follower_id", ForeignKey("users.id")),
+)
+
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+
+
+class Post(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+
+class MyEnum(Enum):
+    one = 1
+    two = 2
+    three = 3
+
+class  Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MyEnum))
+    url = Column(String(250), nullable = False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable = False)
+    post = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable = True)
+    author_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    post_id = Column(Integer, ForeignKey('posts.id'), nullable = False)
+
+
+
+
 
     def to_dict(self):
         return {}
