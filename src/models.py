9d2cf34bda4a1af1d917b076353c6a90fdb132cf
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-07-25 17:33:29 +0000

    done!

--- src/models.py
+++ src/models.py
@@ -1,54 +1,18 @@
-# import os
-# import sys
-# from sqlalchemy import Column, ForeignKey, Integer, String
-# from sqlalchemy.orm import relationship, declarative_base
-# from sqlalchemy import create_engine
-# from eralchemy2 import render_er
-
-# Base = declarative_base()
-
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
-
-# ## Draw from SQLAlchemy base
-# try:
-#     result = render_er(Base, 'diagram.png')
-#     print("Success! Check the diagram.png file")
-# except Exception as e:
-#     print("There was a problem genering the diagram")
-#     raise e
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Table, Enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Enum, Text
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from enum import Enum as PyEnum
 
 Base = declarative_base()
 
 follower = Table(
     "follower",
     Base.metadata,
-    Column("user_id", ForeignKey("users.id")),
-    Column("follower_id", ForeignKey("users.id")),
+    Column("user_from_id", Integer, ForeignKey("users.id")),
+    Column("user_to_id", Integer, ForeignKey("users.id")),
 )
 
 class User(Base):
@@ -62,6 +26,11 @@ class User(Base):
     last_name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
 
+    posts = relationship('Post', back_populates = 'user')
+    comments = relationship('Comment', back_populates = 'user')
+    following = relationship('User', secondary = follower, primaryjoin = id == follower.c.user_from_id, secondaryjoin = id == follower.c.user_to_id, backref = 'followers')
+    
+
 
 
 class Post(Base):
@@ -69,21 +38,26 @@ class Post(Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    image_url = Column(String, nullable = False)
+    caption = Column(Text)
 
-class MyEnum(Enum):
-    one = 1
-    two = 2
-    three = 3
+    user = relationship('User', back_populates = 'posts')
+    comments = relationship('Comment', back_populates  = 'post')
+    media = relationship('Media', back_populates = 'post')
+
+
+class Mediatype(PyEnum):
+    IMAGE = 'image'
+    VIDEO = 'video'
 
 class  Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    type = Column(Enum(MyEnum))
+    type = Column(Enum(Mediatype), nullable = False)
     url = Column(String(250), nullable = False)
     post_id = Column(Integer, ForeignKey('posts.id'), nullable = False)
-    post = relationship(User)
+    post = relationship('Post', back_populates = 'media')
 
 class Comment(Base):
     __tablename__ = 'comments'
@@ -91,6 +65,10 @@ class Comment(Base):
     comment_text = Column(String(250), nullable = True)
     author_id = Column(Integer, ForeignKey('users.id'), nullable = False)
     post_id = Column(Integer, ForeignKey('posts.id'), nullable = False)
+    user = relationship('User', back_populates = 'comments')
+    post = relationship('Post', back_populates = 'comments')
+
+
 
 
 
